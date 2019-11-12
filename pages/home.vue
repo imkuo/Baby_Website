d15edcf3422b72881f7e9b8a56feb82cd7e51164
imkuo <a0916037489@gmail.com>
2019-11-12 21:35:55 +0800

    login and sign-up

---
+++ pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>appear!!!!!!!!!!!!!!!!!</p>
+    </div>
+</template>>
---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyRWT4t_yIJRQr755UEJYdVZ-EYlERdnc",
+    authDomain: "baby-website-f90eb.firebaseapp.com",
+    databaseURL: "https://baby-website-f90eb.firebaseio.com",
+    projectId: "baby-website-f90eb",
+    storageBucket: "baby-website-f90eb.appspot.com",
+    messagingSenderId: "99005700891",
+    appId: "1:99005700891:web:1b1ab567b1a0be7c69d1be"
+}
+
+const app = firebase.initializeApp(firebaseConfig)
+
+export const db = app.database()
