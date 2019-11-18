ad7d74096c7d443188e56323fe25e5f606e736b7
imkuo <a0916037489@gmail.com>
2019-11-18 14:37:35 +0800

    11.18Update2

--- pages/home.vue
+++ pages/home.vue
@@ -2,7 +2,7 @@
     <div>
 <html lang="en">
 
-<head>
+<!--  <head>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,21 +11,21 @@
 
   <title>Agency - Start Bootstrap Theme</title>
 
-  <!-- Bootstrap core CSS -->
+   Bootstrap core CSS 
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom fonts for this template -->
+  Custom fonts for this template 
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
-  <!-- Custom styles for this template -->
+  Custom styles for this template 
   <link href="css/agency.min.css" rel="stylesheet">
   <link href="css/mysetting.css" rel="stylesheet">
 
-</head>
+</head> -->
 
 <body id="page-top">  <!--整個網頁-->
 
@@ -71,7 +71,6 @@
           </li>
            <li class="nav-item">
             <button @click.prevent="logout2"   >Logout </button>
-   
           </li>
         </ul>
       </div>
@@ -164,6 +163,7 @@ export default {
   methods:{
     logout2() {
      this.$store.dispatch("signOut");
+     this.$router.push("/login")
     }
     }
 };
